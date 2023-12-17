18010bdb24f369a58c15a3e1fdfa848557a7a289
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-12-17 21:04:14 +0800

    adjusted InvalidLoginCredentialsModal & SendPostReqSuccessModal

--- becarios-admin/src/components/Modal.jsx
+++ becarios-admin/src/components/Modal.jsx
@@ -4,12 +4,11 @@ const tries = 5;
 
 function InvalidLoginCredentialsModal() {
   return (
-    <div className="bg-brand-red rounded-10 my-4 flex flex-row items-center justify-around px-4 py-2 text-center text-white">
-      <FaRegCircleXmark className="mx-1.5 h-auto w-12" />
-      <p className=" text-[10px] md:text-[12px]">
-        Incorrect username or/and password. You can try again{' '}
-        <strong>{tries} more times </strong>or send a{' '}
-        <strong>forgot password request</strong>.
+    <div className="bg-brand-red rounded-10 my-4 flex flex-row items-center justify-center gap-6 px-4 py-2 text-center text-white">
+      <FaRegCircleXmark className=" h-auto w-8" />
+      <p className=" ml-[-0.5rem] text-[10px] md:text-[12px]">
+        Incorrect username or/and password.<br></br> You can try again
+        <strong> {tries} more times</strong>.
       </p>
     </div>
   );
@@ -17,9 +16,15 @@ function InvalidLoginCredentialsModal() {
 
 function SendPostReqSuccessModal() {
   return (
-    <div className="bg-brand-green rounded-10  my-4 flex flex-row items-center  justify-center gap-2 px-2 py-3 text-center text-white">
-      <FaRegCircleCheck className="mx-1.5 h-auto w-8 md:ml-4" />
-      <p className=" p-0 text-[10px] md:mr-4  md:text-[12px]">
+    // <div className="bg-brand-green rounded-10  my-4 flex flex-row items-center  justify-center gap-2 px-2 py-3 text-center text-white">
+    //   <FaRegCircleCheck className="  mr-[-1rem] h-auto w-8 md:ml-4" />
+    //   <p className=" p-0 text-[10px] md:mr-4  md:text-[12px]">
+    //     Article post request has been approved successfully.
+    //   </p>
+    // </div>
+    <div className="bg-brand-green rounded-10 my-4 flex flex-row items-center justify-center gap-4 px-4 py-2 text-center text-white">
+      <FaRegCircleCheck className=" h-auto w-8 lg:ml-0" />
+      <p className=" ml-0 text-[10px] md:text-[12px] lg:ml-0">
         Article post request has been approved successfully.
       </p>
     </div>
