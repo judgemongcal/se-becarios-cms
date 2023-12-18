0e71fdb47a7405c0fd2ea5b75dfecf0e585a9f37
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-12-18 13:07:32 +0800

    adjusted SendPostReqSuccessModal component

--- becarios-admin/src/components/Modal.jsx
+++ becarios-admin/src/components/Modal.jsx
@@ -16,14 +16,8 @@ function InvalidLoginCredentialsModal() {
 
 function SendPostReqSuccessModal() {
   return (
-    // <div className="bg-brand-green rounded-10  my-4 flex flex-row items-center  justify-center gap-2 px-2 py-3 text-center text-white">
-    //   <FaRegCircleCheck className="  mr-[-1rem] h-auto w-8 md:ml-4" />
-    //   <p className=" p-0 text-[10px] md:mr-4  md:text-[12px]">
-    //     Article post request has been approved successfully.
-    //   </p>
-    // </div>
-    <div className="bg-brand-green rounded-10 my-4 flex flex-row items-center justify-center gap-4 px-4 py-2 text-center text-white">
-      <FaRegCircleCheck className=" h-auto w-8 lg:ml-0" />
+    <div className="bg-brand-green rounded-10 my-4 flex flex-row items-center justify-center gap-2 px-4 py-2 text-center text-white">
+      <FaRegCircleCheck className=" h-auto w-8 md:ml-[1rem] lg:ml-0" />
       <p className=" ml-0 text-[10px] md:text-[12px] lg:ml-0">
         Article post request has been approved successfully.
       </p>
