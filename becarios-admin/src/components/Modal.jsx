ec2cccb931267535598ba6e429ebb01f46066a00
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-12-19 22:22:02 +0800

    added logical rendering and additional styling for ExceededLoginAttemptModal

--- becarios-admin/src/components/Modal.jsx
+++ becarios-admin/src/components/Modal.jsx
@@ -1,11 +1,13 @@
 function ExceededLoginAttemptsModal() {
   return (
-    <div className="mx-auto  mt-[25%] flex flex-col items-center self-center text-center xl:mt-[-2%]">
+    <div className="mx-auto  mt-[35%] flex flex-col items-center self-center text-center md:mt-[20%] xl:mt-[-10%]">
       <img src="./src/assets/org_logo.png" alt="" className="mb-10 w-[25%]" />
-      <h1 className="text-brand-red mx-5 mb-2 text-[1.5rem] font-bold">
+      <h1 className="text-brand-red mx-5 mb-2 text-[1.5rem] font-bold xl:text-[1.85rem]">
         You&apos;ve reached the maximum number<br></br>of failed login attempts
       </h1>
-      <p className="text-[1.25rem] font-medium">Please try again later.</p>
+      <p className="text-[1.25rem] font-medium xl:text-[1.5rem]">
+        Please try again later.
+      </p>
     </div>
   );
 }
