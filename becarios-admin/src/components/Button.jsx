058ca32d84048ac0a7d16b83884e53e7cdc8e7cf
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-12-24 14:52:02 +0800

    created PostReqSucessModalBtn component

--- becarios-admin/src/components/Button.jsx
+++ becarios-admin/src/components/Button.jsx
@@ -82,6 +82,19 @@ function SubmitPostModalBtn() {
   );
 }
 
+function PostReqSucessModalBtn() {
+  return (
+    <div className="flex flex-row justify-around gap-4 py-2">
+      <button className="bg-brand-blue shadow-shadow-db rounded-10 hover:bg-brand-blue-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
+        Create Another
+      </button>
+      <button className="bg-brand-yellow shadow-shadow-db rounded-10 hover:bg-brand-yellow-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
+        Back to Home
+      </button>
+    </div>
+  );
+}
+
 function SubmitEditModalBtn() {
   return (
     <div className="flex flex-row justify-around gap-4 py-2">
@@ -162,6 +175,7 @@ export {
   RejectModalBtn,
   EditArticleBtn,
   SubmitPostModalBtn,
+  PostReqSucessModalBtn,
   SubmitEditModalBtn,
   SubmitDeleteModalBtn,
   ProceedModalBtn,
