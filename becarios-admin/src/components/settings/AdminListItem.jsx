edd5ff69ae397e91ee2df19951b3e4f044a565e8
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-12-26 22:42:30 +0800

    added minor styling

--- becarios-admin/src/components/settings/AdminListItem.jsx
+++ becarios-admin/src/components/settings/AdminListItem.jsx
@@ -3,7 +3,7 @@ import { LuPencil } from 'react-icons/lu';
 
 function AdminListItem() {
   return (
-    <div className="bg-brand-blue rounded-8 flex flex-row items-center justify-between gap-2 p-2">
+    <div className="bg-brand-blue rounded-8 shadow-shadow-db flex flex-row items-center justify-between gap-2 p-2">
       <div className="admin-name ml-4 font-medium">
         <p>John Doe</p>
       </div>
--- becarios-admin/src/components/settings/SuperAdminListItem.jsx
+++ becarios-admin/src/components/settings/SuperAdminListItem.jsx
@@ -2,7 +2,7 @@ import { LuPencil } from 'react-icons/lu';
 
 function SuperAdminListItem() {
   return (
-    <div className="bg-brand-yellow rounded-8 flex flex-row items-center justify-between gap-2 p-2">
+    <div className="bg-brand-yellow rounded-8 shadow-shadow-db flex flex-row items-center justify-between gap-2 p-2">
       <div className="admin-name ml-4 font-medium">
         <p>John Doe</p>
       </div>
