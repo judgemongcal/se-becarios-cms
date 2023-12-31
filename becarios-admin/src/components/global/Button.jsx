2410baf1301dddde1e6e2b499cc6d0c5a864cedc
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-12-31 20:11:32 +0800

    separated Manage Content page buttons from global buttons component

--- becarios-admin/src/components/global/Button.jsx
+++ becarios-admin/src/components/global/Button.jsx
@@ -95,32 +95,6 @@ function PostReqSucessModalBtn() {
   );
 }
 
-function SubmitEditModalBtn() {
-  return (
-    <div className="flex flex-row justify-around gap-4 py-2">
-      <button className="bg-brand-red shadow-shadow-db rounded-10 hover:bg-brand-red-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
-        Go Back
-      </button>
-      <button className="bg-brand-blue shadow-shadow-db rounded-10 hover:bg-brand-blue-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
-        Submit Request
-      </button>
-    </div>
-  );
-}
-
-function SubmitDeleteModalBtn() {
-  return (
-    <div className="flex flex-row justify-around gap-4 py-2">
-      <button className="bg-brand-red shadow-shadow-db rounded-10 hover:bg-brand-red-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
-        Go Back
-      </button>
-      <button className="bg-brand-yellow shadow-shadow-db rounded-10 hover:bg-brand-yellow-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
-        Submit Request
-      </button>
-    </div>
-  );
-}
-
 function ProceedModalBtn() {
   return (
     <button className="bg-brand-yellow shadow-shadow-db rounded-10 hover:bg-brand-yellow-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
@@ -168,22 +142,6 @@ function DeleteAdmiModalBtn() {
   );
 }
 
-function GenerateReportBtn() {
-  return (
-    <button className="bg-brand-green shadow-shadow-db rounded-10 hover:bg-brand-green-dark py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-300">
-      Generate Report
-    </button>
-  );
-}
-
-function BacktoDashboardBtn() {
-  return (
-    <button className="bg-brand-black shadow-shadow-db rounded-10 hover:bg-brand-light py-3 text-[1.15rem] font-semibold text-white duration-300 hover:text-black">
-      Back to Dashboard
-    </button>
-  );
-}
-
 export {
   LoginBtn,
   ViewAllBtn,
@@ -199,6 +157,4 @@ export {
   EditAdminModalBtn,
   DeleteAdmiModalBtn,
   LogoutBtn,
-  GenerateReportBtn,
-  BacktoDashboardBtn,
 };
