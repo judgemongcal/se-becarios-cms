88f72d1261d46e7060383853d15ed3ed5eff6ebd
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-01-04 21:36:58 +0800

    updated NavBar component

--- becarios-admin/src/App.jsx
+++ becarios-admin/src/App.jsx
@@ -8,15 +8,17 @@ import {
 import './App.css';
 import Login from './components/pages/Login';
 import PageNotFound from './components/pages/PageNotFound';
+import NavBar from './components/global/NavBar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="login" element={<Login />} />
+    //     <Route path="*" element={<PageNotFound />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <NavBar />
   );
 }
 
--- becarios-admin/src/components/global/NavBar.jsx
+++ becarios-admin/src/components/global/NavBar.jsx
@@ -1,27 +1,47 @@
+import {
+  HiOutlineViewBoards,
+  HiOutlinePlusCircle,
+  HiOutlineFolderOpen,
+  HiOutlineArchive,
+  HiOutlineCog,
+  HiOutlineLogout,
+} from 'react-icons/hi';
+
 function NavBar() {
   return (
-    <div className="bg-brand-black flex flex-col items-center justify-evenly gap-[1rem] text-white md:w-[17.5rem]">
+    <div className="bg-brand-black  fixed flex h-[100vh] flex-col items-center gap-[3.5rem] text-white md:w-[17.5rem]">
       <img
         src="./src/assets/org_logo.png"
         alt=""
         className="mb-10 mt-[80px] w-[50%]"
       />
       <div className="user-details"></div>
-      <div className="dashboard">
-        <button>Dashboard</button>
-      </div>
-      <div className="manage-content">
-        <button>Manage Content</button>
-      </div>
-      <div className="post-archives">
-        <button>Post Archives</button>
-      </div>
-      <div className="settings">
-        <button>Settings</button>
-      </div>
-      <div className="sign-out">
-        <button>Sign Out</button>
-      </div>
+      <section className="navigation mx-auto flex flex-col items-center justify-center gap-[3rem]">
+        <button className="dashboard hover:fill-brand-yellow hover:text-brand-yellow justify-baseline flex items-center gap-2">
+          <HiOutlineViewBoards className=" h-auto w-[24px]" />
+          <p>Dashboard</p>
+        </button>
+        <button className="create-post hover:fill-brand-yellow hover:text-brand-yellow flex items-center justify-center gap-2">
+          <HiOutlinePlusCircle className="ml-1 h-auto w-[24px]" />
+          <p>Create Post</p>
+        </button>
+        <button className="manage-content hover:fill-brand-yellow hover:text-brand-yellow flex items-center justify-center gap-2">
+          <HiOutlineFolderOpen className="ml-10 h-auto w-[24px]" />
+          <p>Manage Content</p>
+        </button>
+        <div className="post-archives  hover:fill-brand-yellow hover:text-brand-yellow flex items-center justify-center gap-2">
+          <HiOutlineArchive className="ml-5 h-auto w-[24px]" />
+          <p>Post Archives</p>
+        </div>
+        <div className="settings  hover:fill-brand-yellow hover:text-brand-yellow mb-[90px] flex items-center justify-center gap-2">
+          <HiOutlineCog className="-ml-5 h-auto w-[24px]" />
+          <p>Settings</p>
+        </div>
+        <div className="sign-out  hover:fill-brand-yellow hover:text-brand-yellow flex items-center justify-center gap-2">
+          <HiOutlineLogout className="-ml-3.5 -mr-0.5 h-auto w-[24px]" />
+          <p>Sign Out</p>
+        </div>
+      </section>
     </div>
   );
 }
