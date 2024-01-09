64b729714d63ce129b2de76820e5530b47e50806
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-01-09 20:51:47 +0800

    created ActivityList component

---
+++ becarios-admin/src/components/recent-activities/ActivityList.jsx
@@ -0,0 +1,25 @@
+import { ActivityListItem } from './ActivityListItem';
+import { DashboardViewAllBtn } from '../global/Button';
+import { NavLink } from 'react-router-dom';
+
+function ActivityList() {
+  return (
+    <div className="bg-brand-input shadow-shadow-db rounded-8 flex min-w-[100%] flex-col gap-2  lg:max-w-[40%]">
+      <div className="header flex flex-row items-center justify-between p-5 py-4 lg:mt-4 lg:py-3">
+        <h1 className="text-[1.25rem] font-bold lg:text-[1.5rem]">
+          Pending for Approval
+        </h1>
+        <h1 className="bg-brand-red rounded-[3rem] px-3 py-2.5 text-[1.25rem] font-bold tracking-wide text-white">
+          10
+        </h1>
+      </div>
+      <div className="req-items flex flex-col gap-4 p-5">
+        {/* CONVERT INTO ARRAY.MAP */}
+        <ActivityListItem />
+      </div>
+      <DashboardViewAllBtn path="/manage-content" />
+    </div>
+  );
+}
+
+export default ActivityList;
--- becarios-admin/src/pages/Dashboard.jsx
+++ becarios-admin/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import NavBar from '../components/global/NavBar';
 import NavBarMobile from '../components/global/NavBarMobile';
 import RequestList from '../components/manage-content/RequestList';
 import { RequestListItem } from '../components/manage-content/RequestListItem';
+import ActivityList from '../components/recent-activities/ActivityList';
 
 function Dashboard() {
   return (
@@ -16,6 +17,7 @@ function Dashboard() {
       <div className="content flex w-[100%] flex-col gap-[5rem] px-9 md:px-16 lg:mt-[3rem]">
         <Greeting name="Karen" />
         <RequestList />
+        <ActivityList />
       </div>
     </div>
   );
