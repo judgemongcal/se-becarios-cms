6cf00f21b9a4dbe0c1f240ca626d22d79b0c7ca4
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-01-09 23:12:01 +0800

    minor styling and path changes

--- becarios-admin/src/components/recent-activities/ActivityList.jsx
+++ becarios-admin/src/components/recent-activities/ActivityList.jsx
@@ -5,19 +5,18 @@ import { NavLink } from 'react-router-dom';
 function ActivityList() {
   return (
     <div className="bg-brand-input shadow-shadow-db rounded-8 flex min-w-[100%] flex-col gap-2  lg:max-w-[40%]">
-      <div className="header flex flex-row items-center justify-between p-5 py-4 lg:mt-4 lg:py-3">
+      <div className="header mt-2 flex flex-row items-center justify-between p-5 py-4 lg:mt-4 lg:py-3">
         <h1 className="text-[1.25rem] font-bold lg:text-[1.5rem]">
-          Pending for Approval
-        </h1>
-        <h1 className="bg-brand-red rounded-[3rem] px-3 py-2.5 text-[1.25rem] font-bold tracking-wide text-white">
-          10
+          Recent Activities
         </h1>
       </div>
       <div className="req-items flex flex-col gap-4 p-5">
         {/* CONVERT INTO ARRAY.MAP */}
         <ActivityListItem />
+        <ActivityListItem />
+        <ActivityListItem />
       </div>
-      <DashboardViewAllBtn path="/manage-content" />
+      <DashboardViewAllBtn path="/recent-activities" />
     </div>
   );
 }
