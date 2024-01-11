ad91c8c0f8c088fc9e5446fc452e2ade27b0b8c4
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-01-11 20:24:40 +0800

    adjusted screen scaling of SearchField and ContentFilters components

--- becarios-admin/src/components/global/SearchField.jsx
+++ becarios-admin/src/components/global/SearchField.jsx
@@ -7,7 +7,7 @@ import {
 function SearchField() {
   return (
     <div className=" flex w-full justify-evenly gap-3">
-      <div className="search-box bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-light flex w-full flex-row items-center gap-3 p-2.5 duration-300">
+      <div className="search-box bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-light flex w-full flex-row items-center gap-3 p-2.5 duration-300 md:p-4">
         <GoSearch className="fill-brand-gray h-auto w-[36px]" />
         <input
           type="text"
@@ -17,10 +17,10 @@ function SearchField() {
           className="bg-brand-input hover:bg-brand-light w-full text-[18px] xl:mr-12"
         />
       </div>
-      <button className="bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-light p-3 duration-300">
+      <button className="bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-light p-3 duration-300 md:hidden">
         <GoArrowLeft className="fill-brand-gray h-auto w-[24px]" />
       </button>
-      <button className="bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-light p-3 duration-300">
+      <button className="bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-light p-3 duration-300 md:hidden">
         <GoArrowRight className="fill-brand-gray h-auto w-[24px]" />
       </button>
     </div>
--- becarios-admin/src/components/manage-content/ContentFilters.jsx
+++ becarios-admin/src/components/manage-content/ContentFilters.jsx
@@ -10,20 +10,26 @@ import { GoArrowLeft, GoArrowRight } from 'react-icons/go';
 
 function ContentFilters() {
   return (
-    <div className=" mt-2 flex flex-row justify-between gap-2 md:gap-5">
-      <button className="alpha-asc bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-yellow inline-flex flex-row items-center justify-center px-4 py-3 duration-300 md:px-8  md:py-3">
+    <div className=" mt-2 flex w-[100%] flex-row justify-between gap-2  md:gap-5">
+      <button className="alpha-asc bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-yellow inline-flex flex-row items-center justify-center px-[5%] py-3 duration-300   md:py-5">
         <BsSortAlphaUp className="fill-brand-black h-[22px] w-[32px]" />
       </button>
-      <button className="alpha-asc bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-yellow inline-flex flex-row items-center justify-center px-4 py-3 duration-300 md:px-3  md:py-3">
+      <button className="alpha-asc bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-yellow inline-flex flex-row items-center justify-center px-[5%] py-3 duration-300  md:py-5">
         <BsSortAlphaDown className="fill-brand-black h-[22px] w-[28px]" />
       </button>
-      <button className="date-asc bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-yellow inline-flex flex-row items-center justify-center px-4 py-3 duration-300 md:px-3  md:py-3">
+      <button className="date-asc bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-yellow inline-flex flex-row items-center justify-center px-[5%] py-3 duration-300   md:py-5">
         <BsArrowUp className="fill-brand-black ml-[-0.25rem] h-auto w-[20px]" />
         <BsCalendarWeek className="fill-brand-black h-auto w-[16px]" />
       </button>
-      <button className="date-desc bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-yellow inline-flex flex-row items-center justify-center px-4 py-3 duration-300 md:mr-6 md:px-3 md:py-4">
+      <button className="date-desc bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-yellow inline-flex flex-row items-center justify-center px-[5%] py-3 duration-300  md:py-6">
         <BsArrowDown className="fill-brand-black ml-[-0.25rem] h-auto w-[20px]" />
         <BsCalendarWeek className="fill-brand-black h-auto w-[16px]" />
+      </button>
+      <button className="bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-light hidden px-[5%] py-3 duration-300 md:block">
+        <GoArrowLeft className="fill-brand-gray h-auto w-[24px]" />
+      </button>
+      <button className="bg-brand-input rounded-8 shadow-shadow-db hover:bg-brand-light duration-30 hidden px-[5%] py-3 md:block">
+        <GoArrowRight className="fill-brand-gray h-auto w-[24px]" />
       </button>
     </div>
   );
