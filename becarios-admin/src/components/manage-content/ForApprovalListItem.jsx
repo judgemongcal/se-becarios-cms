6e98c32abceac8fb5c4bfdb2ab8f1639139ec026
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-01-12 11:30:49 +0800

    adjusted mobile layout of ForApprovalListItem component

--- becarios-admin/src/components/manage-content/ForApprovalListItem.jsx
+++ becarios-admin/src/components/manage-content/ForApprovalListItem.jsx
@@ -4,27 +4,27 @@ import { ForApprovalListItemBtn } from '../global/Button';
 
 function ForApprovalListItem() {
   return (
-    <div className="req-container shadow-shadow-db rounded-8 flex flex-row gap-2 p-5">
+    <div className="req-container shadow-shadow-db rounded-8 flex flex-col gap-2 p-5">
       <div className="req-img w-full">
         <img
-          src="./src/assets/org_logo.png"
+          src="./src/assets/sample_2.jpeg"
           alt=""
-          className="h-auto w-full"
+          className="rounded-8 h-auto  w-full"
         />
       </div>
-      <div className="req-info">
-        <p className="activity-label bg-brand-yellow rounded-8 mb-3 min-w-[50%] max-w-[200px] px-4 py-1 text-center text-[0.85rem] font-medium">
+      <div className="req-info mt-2 flex flex-col items-center text-center">
+        <p className="activity-label bg-brand-yellow rounded-8 mb-3 min-w-[50%] max-w-[200px] px-4 py-1 text-[0.9rem] font-medium ">
           Article Post Request
         </p>
-        <h1 className="req-title mb-2 text-[0.95rem] font-semibold">
+        <h1 className="req-title mb-2 text-[1.25rem] font-semibold">
           Lorem ipsum dolor sit amet consectetur adipisicing
           elit
         </h1>
-        <div className="time-info mb-1 flex flex-row gap-3 text-[0.75rem]">
+        <div className="time-info mb-1 flex flex-row gap-3 text-[1rem]">
           <p>February 7, 2024</p>
           <p>8:24 AM</p>
         </div>
-        <p className="author text-[0.85rem]">
+        <p className="author text-[1rem]">
           Submitted by: Hannah Yu
         </p>
         <ForApprovalListItemBtn />
