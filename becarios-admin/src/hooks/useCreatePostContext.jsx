0e8c59343e36b4ff48afaebb445b852dc35b436a
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-01-23 19:34:51 +0800

    create custom hook useCreatePostContext

---
+++ becarios-admin/src/hooks/useCreatePostContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState } from 'react';
+
+const CreatePostContext = createContext();
+
+export function useCreatePostContext() {
+  return useContext(CreatePostContext);
+}
+
+export function CreatePostProvider({ children }) {
+  const [articleTitle, setArticleTitle] = useState('');
+
+  const [articleImageFileName, setArticleImageFileName] =
+    useState('');
+  const [articleImageSrc, setArticleImageSrc] =
+    useState('');
+  const [articleBody, setArticleBody] = useState('');
+  const [isSubmitBtnPressed, setIsSubmitBtnPressed] =
+    useState(false);
+  const [isPreview, setIsPreview] = useState(false);
+  const [isSubmitConfirmed, setIsSubmitConfirmed] =
+    useState(false);
+
+  const contextValue = {
+    articleTitle,
+    setArticleTitle,
+    articleImageSrc,
+    setArticleImageSrc,
+    articleBody,
+    setArticleBody,
+    isPreview,
+    setIsPreview,
+    isSubmitBtnPressed,
+    setIsSubmitBtnPressed,
+    isSubmitConfirmed,
+    setIsSubmitConfirmed,
+  };
+
+  return (
+    <CreatePostContext.Provider value={contextValue}>
+      {children}
+    </CreatePostContext.Provider>
+  );
+}
