9bf835f12fb921468ac436b7a31b236a51a3982e
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-01-25 17:49:47 +0800

    added function to hide RemoveAdminModal when btn in RemoveAdminBtn is clicked

--- becarios-admin/src/components/global/Button.jsx
+++ becarios-admin/src/components/global/Button.jsx
@@ -268,12 +268,25 @@ function EditAdminModalBtn() {
 }
 
 function RemoveAdminModalBtn() {
+  const {
+    isRemoveAdminBtnClicked,
+    setIsRemoveAdminBtnClicked,
+  } = useSettingsContext();
+
+  function handleBack(e) {
+    e.preventDefault();
+    setIsRemoveAdminBtnClicked(!isRemoveAdminBtnClicked);
+  }
+
   return (
     <div className="flex flex-row justify-around gap-4 py-2">
       <button className="bg-brand-yellow shadow-shadow-db rounded-10 hover:bg-brand-yellow-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
         Delete Admin
       </button>
-      <button className="bg-brand-red shadow-shadow-db rounded-10 hover:bg-brand-red-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100">
+      <button
+        className="bg-brand-red shadow-shadow-db rounded-10 hover:bg-brand-red-dark w-[100%] py-3 text-[1.15rem] font-semibold text-[#FFFFFF] duration-100"
+        onClick={(e) => handleBack(e)}
+      >
         Go Back
       </button>
     </div>
