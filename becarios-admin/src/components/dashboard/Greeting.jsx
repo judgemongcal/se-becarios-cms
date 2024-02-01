6a0eb5311a513121d2b8634a1bc5e0ddd78aee19
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-02-01 14:35:38 +0800

    updated Greeting component to conditionally render message based on time of the day

--- becarios-admin/src/components/dashboard/Greeting.jsx
+++ becarios-admin/src/components/dashboard/Greeting.jsx
@@ -1,7 +1,16 @@
 function Greeting({ name }) {
+  const currDate = new Date();
+  const currHour = currDate.getHours();
+  console.log(currHour);
+
   return (
     <h1 className=" mt-[10rem] text-[1.5rem] font-bold tracking-wide md:text-[2rem] lg:mt-[2rem]">
-      Good morning, {name}.
+      {currHour < 12
+        ? `Good morning`
+        : currHour >= 12 && currHour < 18
+          ? `Good Afternoon`
+          : `Good Evening`}
+      , {name}.
     </h1>
   );
 }
