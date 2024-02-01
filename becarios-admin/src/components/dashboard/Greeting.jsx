58b0bf7f251ad70820c9874fd88a21b28f424908
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-02-01 15:07:15 +0800

    Added loading state for Greeting component

--- becarios-admin/src/components/dashboard/Greeting.jsx
+++ becarios-admin/src/components/dashboard/Greeting.jsx
@@ -8,12 +8,16 @@ function Greeting() {
 
   return (
     <h1 className=" mt-[10rem] text-[1.5rem] font-bold tracking-wide md:text-[2rem] lg:mt-[2rem]">
-      {currHour < 12
-        ? `Good morning`
-        : currHour >= 12 && currHour < 18
-          ? `Good Afternoon`
-          : `Good Evening`}
-      , {userInfo.first_name}.
+      {userInfo && userInfo.first_name
+        ? `${
+            currHour < 12
+              ? `Good morning`
+              : currHour >= 12 && currHour < 18
+                ? `Good Afternoon`
+                : `Good Evening`
+          }
+      , ${userInfo.first_name}.`
+        : `Loading...`}
     </h1>
   );
 }
