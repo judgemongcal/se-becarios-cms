e87d6fdcac38e6da4b67e4cd7dc5caccd315aec6
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-02-01 14:51:30 +0800

    added conditional rendering for RequestList in dashboard page based on current user's role

--- becarios-admin/src/pages/Dashboard.jsx
+++ becarios-admin/src/pages/Dashboard.jsx
@@ -7,9 +7,11 @@ import RequestList from '../components/manage-content/RequestList';
 import { RequestListItem } from '../components/manage-content/RequestListItem';
 import ActivityList from '../components/recent-activities/ActivityList';
 import { useSignOutContext } from '../hooks/useSignOutContext';
+import { useUserInfoContext } from '../hooks/useUserInfoContext';
 
 function Dashboard() {
   const { isSignOutClicked } = useSignOutContext();
+  const { userInfo } = useUserInfoContext();
   return (
     <div className="flex flex-col justify-start lg:flex-row">
       <div className="navs">
@@ -20,7 +22,9 @@ function Dashboard() {
       <div className="content flex w-[100%] flex-col gap-[5rem] px-9 md:mb-[5rem] md:px-16 lg:ml-[21rem] lg:mt-[3rem]">
         <Greeting name="Karen" />
         <div className="lists flex flex-col gap-[2rem] xl:flex-row">
-          <RequestList />
+          {userInfo.role === 'Super Admin' && (
+            <RequestList />
+          )}
           <ActivityList />
         </div>
       </div>
