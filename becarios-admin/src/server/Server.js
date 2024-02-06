e91f89811ba1ccbd27eab67687e2c2edead8c2f7
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-02-06 16:33:22 +0800

    installed firebase-admin via npm

--- becarios-admin/src/server/Server.js
+++ becarios-admin/src/server/Server.js
@@ -1,11 +1,25 @@
 import './Firebase.js'; // Importing firebase.js here initializes Firebase
 import express from 'express';
 import cors from 'cors';
+import { db } from './Firebase.js';
+import { fetchArticles } from './API/GlobalAPI.js';
 
 const app = express();
 app.use(cors());
 
 const port = 5001;
+
+app.get('/add-admin', async (req, res) => {
+  try {
+    const articleSnapshot = await fetchArticles();
+
+    return articleSnapshot;
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
