d53ad613d92de42909457479c026b9fde152c47b
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-02-13 21:02:21 +0800

    added sorting function for content list

--- becarios-admin/src/server/API/ManageContentAPI.js
+++ becarios-admin/src/server/API/ManageContentAPI.js
@@ -72,3 +72,108 @@ export async function fetchPostedArticles() {
     throw error;
   }
 }
+
+// Fetch Articles by Alphabetical Order
+export async function fetchAllPostedArticlesAZ(
+  sortOrder = 'asc',
+) {
+  try {
+    const colRef = collection(db, 'articles');
+    const q = query(
+      colRef,
+      where('isApproved', '==', true),
+      where('isArchived', '==', false),
+    );
+
+    const filteredArticlesSnapshot = await getDocs(q);
+
+    const allPostedArticles = [];
+
+    filteredArticlesSnapshot.forEach((doc) => {
+      //console.log('Document ID:', doc.id);
+      //console.log('Document Data:', doc.data().title);
+      allPostedArticles.push({
+        data: doc.data(),
+        id: doc.id,
+      });
+    });
+
+    allPostedArticles.sort((a, b) =>
+      sortOrder === 'asc'
+        ? a.data.title.localeCompare(b.data.title, 'en', {
+            sensitivity: 'base',
+          })
+        : b.data.title.localeCompare(a.data.title, 'en', {
+            sensitivity: 'base',
+          }),
+    );
+
+    return allPostedArticles;
+  } catch (error) {
+    console.error(
+      'Error fetching Posted Articles with Details',
+      error,
+    );
+    throw error;
+  }
+}
+
+// Fetch and Sort Articles by Chronological Order
+export async function fetchAllPostedArticles09(
+  sortOrder = 'asc',
+) {
+  try {
+    const colRef = collection(db, 'articles');
+    const q = query(
+      colRef,
+      where('isApproved', '==', true),
+      where('isArchived', '==', false),
+    );
+
+    const filteredArticlesSnapshot = await getDocs(q);
+
+    const allPostedArticles = [];
+
+    filteredArticlesSnapshot.forEach((doc) => {
+      console.log('Document ID:', doc.id);
+      console.log('Document Date:', doc.data().datePosted);
+      const datePosted = doc.data().datePosted;
+      allPostedArticles.push({
+        data: doc.data(),
+        id: doc.id,
+      });
+    });
+
+    // Sort the array based on datePosted
+    if (sortOrder === 'asc') {
+      allPostedArticles.sort((a, b) => {
+        return (
+          a.data.datePosted.toMillis() -
+          b.data.datePosted.toMillis()
+        );
+      });
+    } else {
+      allPostedArticles.sort((a, b) => {
+        return (
+          b.data.datePosted.toMillis() -
+          a.data.datePosted.toMillis()
+        );
+      });
+    }
+    // For Testing
+    console.log(
+      'Fetched Sorted Articles By Date:',
+      allPostedArticles.map(
+        (article) => article.data.datePosted,
+      ),
+    );
+
+    return allPostedArticles;
+  } catch (error) {
+    console.error(
+      'Error fetching Posted Articles with Details',
+      error,
+    );
+    throw error;
+  }
+}
