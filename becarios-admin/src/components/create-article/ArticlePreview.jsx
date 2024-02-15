8ecac3f40e047770185c86497fc78a801572354c
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-02-15 14:13:37 +0800

    adjusted styling of ArticlePreview content

--- becarios-admin/src/components/create-article/ArticlePreview.jsx
+++ becarios-admin/src/components/create-article/ArticlePreview.jsx
@@ -11,13 +11,22 @@ function ArticlePreview() {
   const parsedContent = content.body.innerHTML;
 
   return (
-    <div className="Preview flex flex-col gap-4">
-      <h1 className="Title bg-red  mb-4 font-bold text-black">
+    <div className="Preview mx-auto flex  w-[80%] flex-col gap-4 ">
+      <h1 className="Title bg-red  mb-4 self-center font-bold text-black">
         {articleTitle}
       </h1>
-      <img src={articleImageSrc} alt="" />
+      <div className=" mb-5 flex h-auto max-h-[500px] max-w-[1000px] self-center">
+        <img
+          src={articleImageSrc}
+          alt=""
+          className="rounded-8 shadow-shadow-db"
+        />
+      </div>
       <div
-        dangerouslySetInnerHTML={{ __html: parsedContent }}
+        dangerouslySetInnerHTML={{
+          __html: parsedContent,
+        }}
+        className=""
       />
     </div>
   );
