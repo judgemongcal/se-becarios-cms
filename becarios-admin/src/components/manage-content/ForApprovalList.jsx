895f788501b9217b8ee5e6b3c29ead02392e6c03
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-02-17 22:55:42 +0800

    fixed pending list count indicator styling

--- becarios-admin/src/components/manage-content/ForApprovalList.jsx
+++ becarios-admin/src/components/manage-content/ForApprovalList.jsx
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 import * as ManageContentAPI from '../../server/API/ManageContentAPI';
 
 function ForApprovalList() {
-  const [pendingArticleCount, setPendingArticleCount] = useState(null);
+  const [pendingArticleCount, setPendingArticleCount] =
+    useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const count = await ManageContentAPI.getCurrentPendingArticleCount();
+        const count =
+          await ManageContentAPI.getCurrentPendingArticleCount();
         setPendingArticleCount(count);
       } catch (error) {
         // Handle error
@@ -23,9 +25,11 @@ function ForApprovalList() {
         <h1 className="text-[1.25rem] font-bold lg:text-[1.5rem]">
           Pending for Approval
         </h1>
-        <h1 className="bg-brand-red rounded-[3rem] px-3 py-2.5 text-[1.25rem] font-bold tracking-wide text-white">
-          {pendingArticleCount !== null ? pendingArticleCount : 'Loading...'}
-        </h1>
+        {pendingArticleCount && (
+          <h1 className="bg-brand-red flex h-12 w-12 items-center justify-center rounded-[100%] p-4 text-[1.5rem] font-bold tracking-wide text-white">
+            {pendingArticleCount}
+          </h1>
+        )}
       </div>
       <div className="req-items -mt-5 mb-4 flex flex-col gap-6 p-8 xl:grid xl:grid-cols-2">
         {/* CONVERT INTO ARRAY.MAP */}
